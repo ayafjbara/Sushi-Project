fae8be247c7119e16d93f6ff53d56e8103c08b09
netanelf <netanelf@cs.huji.ac.il>
2019-11-17 16:58:21 +0200

    welcome screen draft

--- app/src/main/java/com/ux/itamae/MainActivity.java
+++ app/src/main/java/com/ux/itamae/MainActivity.java
@@ -2,7 +2,11 @@ package com.ux.itamae;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton maki_btn = findViewById(R.id.maki_button);
+        ImageButton photomaki_btn = findViewById(R.id.photomaki_button);
+
+        final Context context = this;
+
+        maki_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implement this
+                Toast.makeText(context, "order my maki", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        photomaki_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implement this
+                Toast.makeText(context, "order my Photo-maki", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
