6062cfb7f186d6ace09a86ab6a726ba23ba1cd2b
Aya <aya.jbara@mail.huji.ac.il>
2019-11-25 16:32:45 +0200

    Updated on InExtra clicklistener

--- app/src/main/java/com/ux/itamae/MainActivity.java
+++ app/src/main/java/com/ux/itamae/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/ux/itamae/SushiExtrasActivity.java
+++ app/src/main/java/com/ux/itamae/SushiExtrasActivity.java
@@ -39,25 +39,41 @@ public class SushiExtrasActivity extends AppCompatActivity implements GridExtraA
         sushiImage = findViewById(R.id.image_sushi);
     }
 
+    public void onInExtraClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_in_top_right:
+                extra1.setVisibility(View.INVISIBLE);
+                extra1=null;
+                break;
+            case R.id.img_in_top_left:
+                extra2.setVisibility(View.INVISIBLE);
+                extra2 = null;
+                break;
+            case R.id.img_in_bottom:
+                extra3.setVisibility(View.INVISIBLE);
+                extra3 = null;
+                break;
+        }
+    }
 
     @Override
     public void onExtraClick(int image) {
         if (extra1 == null) {
             extra1 = findViewById(R.id.img_in_top_right);
-            extra1.setImageDrawable(get_in_extra(image));
+            extra1.setImageDrawable(geInExtra(image));
             extra1.setVisibility(View.VISIBLE);
         } else if (extra2 == null) {
             extra2 = findViewById(R.id.img_in_top_left);
-            extra2.setImageDrawable(get_in_extra(image));
+            extra2.setImageDrawable(geInExtra(image));
             extra2.setVisibility(View.VISIBLE);
         } else if (extra3 == null) {
             extra3 = findViewById(R.id.img_in_bottom);
-            extra3.setImageDrawable(get_in_extra(image));
+            extra3.setImageDrawable(geInExtra(image));
             extra3.setVisibility(View.VISIBLE);
         }
     }
 
-    private Drawable get_in_extra(int image) {
+    private Drawable geInExtra(int image) {
         switch (image) {
             case R.drawable.ic_ex_avocado:
                 return getDrawable(R.drawable.ic_in_avocado);
